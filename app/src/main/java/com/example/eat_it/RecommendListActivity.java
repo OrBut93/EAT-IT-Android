6eae11ca9a0089cbe0d623bb60d512a6c7c8ca85
orow1993 <orow1993@gmail.com>
2020-08-06 13:53:19 +0300

    recycle view done

--- app/src/main/java/com/example/eat_it/RecommendListActivity.java
+++ app/src/main/java/com/example/eat_it/RecommendListActivity.java
@@ -7,51 +7,84 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.eat_it.model.Model;
+import com.example.eat_it.model.Recommend;
+
+import java.util.List;
 
 public class RecommendListActivity extends AppCompatActivity {
 
     RecyclerView list;
+    List<Recommend> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_list);
+        data= Model.instance.getAllRec();
 
         list = findViewById(R.id.recommend_list);
         list.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         list.setLayoutManager(layoutManager);
+
+       MyAdapter adapter = new MyAdapter();
+       list.setAdapter(adapter);
     }
 
     static class RecommendViewHolder extends RecyclerView.ViewHolder{
 
+        TextView titleTv;
+        TextView locationTv;
+        TextView descriptionTv;
+        ImageView imageView;
+
         public RecommendViewHolder(@NonNull View itemView) {
             super(itemView);
+            titleTv = itemView.findViewById(R.id.row_title_tv);
+            locationTv = itemView.findViewById(R.id.row_location_tv);
+            descriptionTv = itemView.findViewById(R.id.row_descroption_tv);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+
+        void bind(Recommend recommend){
+            titleTv.setText(recommend.title);
+            locationTv.setText(recommend.location);
+            descriptionTv.setText(recommend.description);
         }
     }
 
     class MyAdapter extends RecyclerView.Adapter<RecommendViewHolder>{
 
-
-
         @NonNull
         @Override
         // what happen when create a view of row
-        public RecommendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+
+        public RecommendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_row, viewGroup, false);
+            RecommendViewHolder viewHolder = new RecommendViewHolder(view);
+            return viewHolder;
         }
 
         //take a row and connect her data
         @Override
         public void onBindViewHolder(@NonNull RecommendViewHolder holder, int position) {
+            Recommend recommend = data.get(position);
+            holder.bind(recommend);
+
+
 
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return data.size();
         }
     }
 }
