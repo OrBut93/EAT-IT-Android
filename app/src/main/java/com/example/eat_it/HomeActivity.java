146384f5221fdc08d1ab96d280e2369200d68fbb
orow1993 <orow1993@gmail.com>
2020-08-09 22:40:27 +0300

    navigation bar complete

--- app/src/main/java/com/example/eat_it/HomeActivity.java
+++ app/src/main/java/com/example/eat_it/HomeActivity.java
@@ -3,25 +3,51 @@ package com.example.eat_it;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.eat_it.model.Recommend;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity implements RecListFragment.Delegate{
 
+    NavController navCtrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        navCtrl = Navigation.findNavController(this, R.id.home_nav_host);
+        NavigationUI.setupActionBarWithNavController(this,navCtrl);
+
+        BottomNavigationView bottonNav = findViewById(R.id.home_bottom_nav);
+        NavigationUI.setupWithNavController(bottonNav, navCtrl);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            navCtrl.navigateUp();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onItemSelected(Recommend recommend) {
         NavController navCtrl = Navigation.findNavController(this, R.id.home_nav_host);
 //        navCtrl.navigate(R.id.action_recListFragment_to_recommendDetailsFragment);
-        RecListFragmentDirections.ActionRecListFragmentToRecommendDetailsFragment2 directions = RecListFragmentDirections.actionRecListFragmentToRecommendDetailsFragment2(recommend);
+//        RecListFragmentDirections.ActionRecListFragmentToRecommendDetailsFragment2 directions = RecListFragmentDirections.actionRecListFragmentToRecommendDetailsFragment2(recommend);
+//        navCtrl.navigate(directions);
+
+        NavGraphDirections.ActionGlobalRecommendDetailsFragment directions = RecListFragmentDirections.actionGlobalRecommendDetailsFragment(recommend);
         navCtrl.navigate(directions);
+
 
     }
 }
