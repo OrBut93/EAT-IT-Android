63642490c489fecfd5adb249cd833f86863f40bd
nir.chetrit <nir.chetrit@gmail.com>
2020-08-11 23:13:44 +0300

    upload image

--- app/src/main/java/com/example/eat_it/model/StoreModel.java
+++ app/src/main/java/com/example/eat_it/model/StoreModel.java
@@ -1,4 +1,32 @@
 package com.example.eat_it.model;
 
 public class StoreModel {
+	
+	public class Listener{
+		void onSuccess(String url);
+		void onFail();
+	}
+	
+	public void saveImage(Bitmap imageBmp, String name, final Listener listener){
+		 
+		FirebaseStorage storage = FirebaseStorage.getInstance();
+		 
+		 StorageReference imagesRef = storage.getReference().child("images").child(name);
+		 ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		 imageBmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+		 byte[] data = baos.toByteArray();
+		 UploadTask uploadTask = imagesRef.putBytes(data);
+		 uploadTask.addOnFailureListener(new OnFailureListener() {
+		 @Override
+		 public void onFailure(Exception exception) {
+		 listener.fail();
+		 }
+		 }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+		 @Override
+		 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+		 @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+		 listener.complete(downloadUrl.toString());
+		 }
+		 });
+		}
 }
